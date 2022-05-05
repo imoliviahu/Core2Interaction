b7e953daf7b0cb406133d6b0a429672c0c694c14
imoliviahu <huo282@newschool.edu>
2022-05-05 15:54:27 -0400

    input/output upload

---
+++ projects/input:output/main.js
@@ -0,0 +1,24 @@
+
+// let myCursor = document.querySelector(".customCursor");
+
+// window.addEventListener("mousemove", function(event){
+//   console.log(event);
+//   console.log("X:" , event.pageX);
+//   console.log("Y:" , event.pageY);
+//   console.log(myCursor);
+//   myCursor.style.top = event.pageY + "px";
+//   myCursor.style.left = event.pageX + "px";
+// });
+
+// EXAMPLE 3 Change position of cursor and adjust alignment of cursor
+let myCursor = document.querySelector(".customCursor");
+
+window.addEventListener("mousemove", function(event){
+  console.log(event);
+  console.log("X:" , event.pageX);
+  console.log("Y:" , event.pageY);
+  console.log(myCursor);
+  //subtract half the heigth and width from each
+  myCursor.style.top = event.pageY - 50 + "px";
+  myCursor.style.left = event.pageX - 50 + "px";
+});
